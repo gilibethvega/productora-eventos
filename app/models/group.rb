6eaad96184744545b3dbd77e6ceff11232469b2d
Gilibeth Vega <gilibethvega@gmail.com>
2021-01-23 18:57:03 -0300

    se realizan cambios en modelo y se fija ruta principal

--- app/models/group.rb
+++ app/models/group.rb
@@ -12,31 +12,28 @@ class Group < ApplicationRecord
   end
 
   def number_concerts
-    Concert.where(group_id: id).count
+    Concert.references(:groups).where(group_id: id).count
   end
 
   def number_people
-    Concert.where(group_id: id).sum(:assistance)
+    Concert.references(:groups).where(group_id: id).sum(:assistance)
   end
 
   def concert_this_month
-    @concerts= Concert.where(group_id: id)
-    month = @concerts.map { |concert| concert.date.month }.count(Time.now.month)
+    @concerts= Concert.references(:groups).where(group_id: id)
+    @concerts.map { |concert| concert.date.month }.count(Time.now.month)
   end
 
   def last_concert
-    @concerts= Concert.where(group_id: id)
     @concerts.map { |concert| (concert.date).strftime("%Y-%B-%A") }.last
   end
 
   def max_people
-    @concerts= Concert.includes(:group).where(group_id: id)
     @concerts.map { |concert| (concert.assistance) }.max
 
   end
 
   def max_time
-    @concerts= Concert.includes(:group).where(group_id: id)
     @concerts.map { |concert| (concert.duration) }.max
   end
 
